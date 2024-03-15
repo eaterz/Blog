a72aa524f7d507a95ac30ff6187f65a0bc247e32
eaterz <muraskoruslans@gmail.com>
2024-03-15 10:02:45 +0200

    commit

--- index.php
+++ index.php
@@ -5,12 +5,11 @@ $url =parse_url($_SERVER["REQUEST_URI"]) ["path"];
 
 if($url == "/"){
     require "controllers/index.php";
-}
-
-if($url == "/about"){
+}else if($url == "/about"){
     require "controllers/about.php";
-}
-
-if($url == "/story"){
+}else if($url == "/story"){
     require "controllers/story.php";
+}else{
+    http_response_code(404);
+    require "controllers/404.php";
 }
--- view/components/navbar.php
+++ view/components/navbar.php
@@ -1,6 +1,9 @@
 
+
 <nav>
+<div class="link">
     <a href="/" class="nav-link">Posts</a>
     <a href="/about" class="nav-link">About us</a>
     <a href="/story" class="nav-link">story</a>
+</div>
 </nav>
